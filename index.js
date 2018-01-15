8b0360ef3f5ecd8c80def0b86538589f499c6653
James Drawneek <jmdrawneek@googlemail.com>
2018-01-15 23:14:59 +0000

    Sort out indenting.

--- index.js
+++ index.js
@@ -22,85 +22,73 @@ module.exports = class DeploymentTools {
   }
 
   processIncommingGitHook() {
-      let errMsg = null;
-      const headers = this.event.headers;
-      const sig = headers['X-Hub-Signature'];
-      const githubEvent = headers['X-GitHub-Event'];
-      const id = headers['X-GitHub-Delivery'];
-      const calculatedSig = this.signRequestBody(this.token, this.event.body);
+    let errMsg = null;
+    const headers = this.event.headers;
+    const sig = headers['X-Hub-Signature'];
+    const githubEvent = headers['X-GitHub-Event'];
+    const id = headers['X-GitHub-Delivery'];
+    const calculatedSig = this.signRequestBody(this.token, this.event.body);
 
-      if (typeof this.token !== 'string') {
-        errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
-        console.log(errMsg);
-        return callback(null, {
-          statusCode: 401,
-          headers: { 'Content-Type': 'text/plain' },
-          body: errMsg,
-        });
-      }
-
-      if (!sig) {
-        errMsg = 'No X-Hub-Signature found on request';
-        console.log(errMsg);
-        return callback(null, {
-          statusCode: 401,
-          headers: { 'Content-Type': 'text/plain' },
-          body: errMsg,
-        });
-      }
-
-      if (!githubEvent) {
-        errMsg = 'No X-Github-Event found on request';
-        console.log(errMsg);
-        return callback(null, {
-          statusCode: 422,
-          headers: { 'Content-Type': 'text/plain' },
-          body: errMsg,
-        });
-      }
-
-      if (!id) {
-        errMsg = 'No X-Github-Delivery found on request';
-        console.log(errMsg);
-        return callback(null, {
-          statusCode: 401,
-          headers: { 'Content-Type': 'text/plain' },
-          body: errMsg,
-        });
-      }
-
-      if (sig !== calculatedSig) {
-        errMsg = 'X-Hub-Signature incorrect. Github webhook token doesn\'t match';
-        console.log(errMsg);
-        return callback(null, {
-          statusCode: 401,
-          headers: { 'Content-Type': 'text/plain' },
-          body: errMsg,
-        });
-      }
-
-      /* eslint-disable */
-      console.log('---------------------------------');
-      console.log(`Github-Event: "${githubEvent}" with action: "${this.event.body.action}"`);
-      console.log('---------------------------------');
-      console.log('Payload', this.event.body);
-      /* eslint-enable */
-
-
-      const filedAdded = this.getFilesFromGit(this.uri);
-      console.log(`${filedAdded} x files added.`);
-
-      console.log(this.files);
-
-        const response = {
-          statusCode: 200,
-          body: JSON.stringify({
-            input: this.event
-          })
-        };
-
-        return this.callback(null, response);
+    if (typeof this.token !== 'string') {
+      errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
+      console.log(errMsg);
+      return callback(null, {
+        statusCode: 401,
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMsg,
+      });
     }
+
+    if (!sig) {
+      errMsg = 'No X-Hub-Signature found on request';
+      console.log(errMsg);
+      return callback(null, {
+        statusCode: 401,
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMsg,
+      });
+    }
+
+    if (!githubEvent) {
+      errMsg = 'No X-Github-Event found on request';
+      console.log(errMsg);
+      return callback(null, {
+        statusCode: 422,
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMsg,
+      });
+    }
+
+    if (!id) {
+      errMsg = 'No X-Github-Delivery found on request';
+      console.log(errMsg);
+      return callback(null, {
+        statusCode: 401,
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMsg,
+      });
+    }
+
+    if (sig !== calculatedSig) {
+      errMsg = 'X-Hub-Signature incorrect. Github webhook token doesn\'t match';
+      console.log(errMsg);
+      return callback(null, {
+        statusCode: 401,
+        headers: { 'Content-Type': 'text/plain' },
+        body: errMsg,
+      });
+    }
+
+    /* eslint-disable */
+    console.log('---------------------------------');
+    console.log(`Github-Event: "${githubEvent}" with action: "${this.event.body.action}"`);
+    console.log('---------------------------------');
+    console.log('Payload', this.event.body);
+    /* eslint-enable */
+
+
+    return this.getFilesFromGit(this.uri);
+  }
 
   signRequestBody(key, body) {
     let hmac = crypto.createHmac("sha1", key);
@@ -118,12 +106,12 @@ module.exports = class DeploymentTools {
     return new Promise((resolve, reject) => {
 
       request(target, (error, response, body) => {
-      if (error) {
-        this.callback(error, `Fetching the resources from: ${downloadsUrl} failed.`);
-      }
+        if (error) {
+          this.callback(error, `Fetching the resources from: ${downloadsUrl} failed.`);
+        }
 
-      const bodyObj = JSON.parse(body);
-      console.log(bodyObj);
+        const bodyObj = JSON.parse(body);
+        console.log(bodyObj);
 
         bodyObj.forEach((fileObject, index) => {
           this.files.push(fileObject);
@@ -133,8 +121,6 @@ module.exports = class DeploymentTools {
           }
         });
       })
-
-
     });
   }
 
@@ -168,20 +154,31 @@ module.exports = class DeploymentTools {
     const parts = filename.split('.');
     console.log(filename.split('.')[parts.length - 1]);
     switch (filename.split('.')[parts.length - 1]) {
-    case 'png':
-    return "image/png";
-    case 'gif':
-    return "image/gif";
-    case 'html':
-    return "text/html";
-    case 'js':
-    return "application/javascript";
-    case 'css':
-    return "text/css";
-    case 'sass':
-    return "text/css";
-    case 'svg':
-    return "image/svg+xml";
+      case 'png':
+        return "image/png";
+      case 'gif':
+        return "image/gif";
+      case 'html':
+        return "text/html";
+      case 'js':
+        return "application/javascript";
+      case 'css':
+        return "text/css";
+      case 'sass':
+        return "text/css";
+      case 'svg':
+        return "image/svg+xml";
+    }
   }
-}
+
+  closeTask() {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        input: this.event
+      })
+    };
+
+    return this.callback(null, response);
+  }
 }
